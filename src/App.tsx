cf76a6432c3df9f51fc0367856db447e9714826a
Divyaraj <divyarajbaraiya05@gmail.com>
2023-04-12 18:41:17 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="navbar">
+          <div className="hamburger-icon" />
+
+          <div className="user-profile-container">
+            <span className="separator"></span>
+
+            <div className="user-profile">
+              <div className="icon"></div>
+              <span className="name">James Smith</span>
+            </div>
+          </div>
+        </div>
       </header>
+
+      <main className="App-main">
+        <div className="input-placeholder">
+          <div className="input-placeholder-content">
+            <span>
+              <img src="../images/Vector-search.png" alt="Img 1" />
+            </span>
+
+            <input
+              className="form-control"
+              type="text"
+              name="searchName"
+              id="searchName"
+              placeholder="Search By RoleName"
+            />
+          </div>
+
+          <span>
+            <img src="../images/Vector-search.png" alt="Img 2" />
+          </span>
+        </div>
+      </main>
     </div>
   );
 }
